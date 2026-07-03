f223abe0be379188276a7ac8d5ab39a9db8998b9
mwa902 <ahmed.wahad213@gmail.com>
2026-07-03 17:04:40 +0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
+<<<<<<< Updated upstream
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
+=======
+import { StrictMode } from "react";
+import { createRoot } from "react-router-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+>>>>>>> Stashed changes
 import { BrowserRouter as Router } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <App />
     </Router>
   </StrictMode>,
-)
+);
