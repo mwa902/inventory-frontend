064404a0818abba0979596cbfd53076db27ec6b3
mwa902 <ahmed.wahad213@gmail.com>
2026-07-04 01:19:11 +0500

    updated route of frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
-<<<<<<< Updated upstream
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client';
-import './index.css'
-import App from './App.jsx'
-=======
 import { StrictMode } from "react";
-import { createRoot } from "react-router-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
->>>>>>> Stashed changes
 import { BrowserRouter as Router } from "react-router";
 
 createRoot(document.getElementById("root")).render(
